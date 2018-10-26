6a139e9bec58e41a7bf97316329ec0a44459120a
MaicolSilvera94 <43857004+MaicolSilvera94@users.noreply.github.com>
2018-10-26 15:48:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -36,13 +36,13 @@
                       <tr>
                        <td>2</td>
                          <td>Juz. del Norte</td>
-                         <td>10</td>
+                         <td>13</td>
                          <td>5</td>
                          <td>+14</td>
                        </tr>
                        <td>3</td>
                          <td>Hernandarias</td>
-                         <td>13</td>
+                         <td>10</td>
                          <td>6</td>
                          <td>+10</td>
                        </tr>
